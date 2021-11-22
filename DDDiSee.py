2de7524c51f66c1d82b4162a6bc3d073f1e56a68
Patrik Sokolowski <42688341+PatrikSo@users.noreply.github.com>
2021-11-22 15:20:07 -0500

    Add files via upload

---
+++ DDDiSee.py
@@ -0,0 +1,18 @@
+'''
+DDD iSee 
+'''
+
+import webbrowser
+
+webbrowser.open('https://www.reddit.com/r/Art/')
+webbrowser.open_new_tab('https://www.tumblr.com/blog/drawndoodaads')
+webbrowser.open_new_tab('https://www.deviantart.com/drawndoodaads')
+webbrowser.open_new_tab('https://mail.google.com/mail/u/1/#inbox')
+webbrowser.open_new_tab('https://www.youtube.com/channel/UC2arcmzI6KqRmSAOyR9UBVw')
+webbrowser.open_new_tab('https://www.twitch.tv/drawndoodaads')
+webbrowser.open_new_tab('https://www.instagram.com/')
+
+
+'''
+https://docs.python.org/3/library/webbrowser.html
+'''
